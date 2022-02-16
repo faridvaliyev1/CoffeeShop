6ae39e6d0909b97ceb79e28d72efd84e5e82de22
faridvaliyev <faridvaliyevgit@gmail.com>
2022-02-17 01:14:24 +0200

    Adding fake json data

---
+++ data/products.go
@@ -0,0 +1,35 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "It is delicious",
+		Price:       2.45,
+		SKU:         "123abd",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short description",
+		Price:       1.99,
+		SKU:         "fgg123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
