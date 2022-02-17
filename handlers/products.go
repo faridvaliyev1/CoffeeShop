36c02ae7f144b4156ed252f11c3713e81ffd6a78
faridvaliyev <faridvaliyevgit@gmail.com>
2022-02-17 02:07:13 +0200

    Adding handlers functionality for getting product

--- handlers/products.go
+++ handlers/products.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/faridvaliyev1/v2/data"
 )
 
 type Products struct {
@@ -14,5 +17,11 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	d, err := json.Marshal(lp)
 
+	if err != nil {
+		http.Error(rw, "Unable to marshall json", http.StatusInternalServerError)
+	}
+	rw.Write(d)
 }
